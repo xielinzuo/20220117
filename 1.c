02ac1685a2c7ea1c57bdb06cd62b64bdbe9c12b7
谢林佐 <88640369+xielinzuo@users.noreply.github.com>
2022-01-17 13:41:59 +0800

    Update 1.c

--- 1.c
+++ 1.c
@@ -2,5 +2,9 @@
 int main(int argc, const char *argv[])
 {
 	printf("hello world\n");
+	printf("hello world\n");
+	printf("hello world\n");
+	printf("hello world\n");
+	printf("hello world\n");
 	return 0;
 }
